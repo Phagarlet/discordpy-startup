77f292ba11e53ffcbcb37d47f138b9c2beefe17e
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-06-11 10:11:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,16 +23,11 @@ cursor = con.cursor()
 
 
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-
-#@bot.command()
-#async def ping(ctx):
-#    await ctx.send('pong')
 
 # 起動時に動作する処理
 @client.event
