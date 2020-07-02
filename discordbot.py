8fb5b43b94253c1dc3ab8f7d51e6815e52518946
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-07-02 16:39:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -274,7 +274,7 @@ async def on_message(message):
         cursor.execute("update PLdata set Wtotal=(%s) where ID=(%s)",(LWt,LID))#Wtotal
         cursor.execute("update PLdata set Wlose=(%s) where ID=(%s)",(LWl,LID))#Wlose
         con.commit()
-        cursor.execute("SELECT * FROM data_set order by ID")
+        cursor.execute("SELECT * FROM PLdata order by ID")
 
 #レート一覧表示 以下管理技士専用コマンド
     if 'Rupdate'in message.content:#レート更新
