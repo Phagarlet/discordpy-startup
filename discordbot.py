f71384de1fe76ffd848e6a8c2ccf4be1ab033eec
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-07-02 10:43:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -155,8 +155,9 @@ async def on_message(message):
                 PLID=cursor.fetchall()[i][1]
                 IDlist.append([PLID,PLname])
                 IDlist.sort(key=lambda x:x[0],reverse=False)#IDソート
+                 await message.channel.send(IDlist)
                 for i in range(len(allPL)):
-                     await message.channel.send(IDlist[i])
+                    await message.channel.send(IDlist[i])
                 await message.channel.send('出力完了です')
         else:
             await message.channel.send('管理技士専用コマンドです')
