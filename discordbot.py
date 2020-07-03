efff8c097ffb6619c4833077636677bdd949287f
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-07-04 00:37:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -298,7 +298,7 @@ async def on_message(message):
         if len(delet)==0:
             cursor.execute("select * from history")
             Num=(len(cursor.fetchall())-1)
-            cursor.execute("insert into history values ((%s),(%s),(%s),(%s),(%s),(%s),(%s))",(num+1,Wname,WID,Lname,LID,WG,LG))
+            cursor.execute("insert into history values ((%s),(%s),(%s),(%s),(%s),(%s),(%s))",(Num+1,Wname,WID,Lname,LID,WG,LG))
             con.commit()
         #ソート
         cursor.execute("SELECT * FROM PLdata order by ID")
