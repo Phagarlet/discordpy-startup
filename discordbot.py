9604307dfa914fbbcd8530e4b11c601ea3da5ffd
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-07-06 22:09:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -523,29 +523,29 @@ async def on_message(message):
             cursor.execute("SELECT * FROM history")#Lcount
             Lcount=cursor.fetchall()[MID][6]
 
-        try:
-            DWID/int(WinID)
-            DLID/int(LoseID)
-            DWres/Wcount
-            DLres/Lcount
-        except ZeroDivisionError:
-        
-            cursor.execute("SELECT * FROM PLdata")#勝者名前取得
-            Wname=cursor.fetchall()[WinID][0]
-            cursor.execute("SELECT * FROM PLdata")#敗者名前取得
-            Lname=cursor.fetchall()[LoseID][0]
+            try:
+                DWID/int(WinID)
+                DLID/int(LoseID)
+                DWres/Wcount
+                DLres/Lcount
+            except ZeroDivisionError:
 
-            #修正上書き
-            cursor.execute("update history set Wname=(%s) where MID=(%s)",(Wname,MID))#Wname
-            cursor.execute("update history set WinID=(%s) where MID=(%s)",(NWID,MID))#WinID
-            cursor.execute("update history set Lname=(%s) where MID=(%s)",(Lname,MID))#Lname
-            cursor.execute("update history set LoseID=(%s) where MID=(%s)",(NLID,MID))#LoseID
-            cursor.execute("update history set Wcount=(%s) where MID=(%s)",(NWres,MID))#Wcount
-            cursor.execute("update history set Lcount=(%s) where MID=(%s)",(NLres,MID))#Lcount
-            con.commit()
-            
-        else:
-            await message.channel.send('構文エラーです。\n情報不一致エラー')
+                cursor.execute("SELECT * FROM PLdata")#勝者名前取得
+                Wname=cursor.fetchall()[WinID][0]
+                cursor.execute("SELECT * FROM PLdata")#敗者名前取得
+                Lname=cursor.fetchall()[LoseID][0]
+
+                #修正上書き
+                cursor.execute("update history set Wname=(%s) where MID=(%s)",(Wname,MID))#Wname
+                cursor.execute("update history set WinID=(%s) where MID=(%s)",(NWID,MID))#WinID
+                cursor.execute("update history set Lname=(%s) where MID=(%s)",(Lname,MID))#Lname
+                cursor.execute("update history set LoseID=(%s) where MID=(%s)",(NLID,MID))#LoseID
+                cursor.execute("update history set Wcount=(%s) where MID=(%s)",(NWres,MID))#Wcount
+                cursor.execute("update history set Lcount=(%s) where MID=(%s)",(NLres,MID))#Lcount
+                con.commit()
+
+            else:
+                await message.channel.send('構文エラーです。\n情報不一致エラー')
 
 
     #通称リセットコマンド
