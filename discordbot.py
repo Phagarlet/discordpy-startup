d95f2c72f53121fd9bd060e63af8abe3c47e1ac6
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-08-06 13:07:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -80,7 +80,7 @@ async def on_message(message):
             cursor.execute("select * from PLdata order by ID")
             allPL=cursor.fetchall()
             for i in range(len(allPL)):
-                cursor.execute("select * from TTQual where PLID=(%s)",(i))
+                cursor.execute("select * from TTQual where PLID=(%s)",(int(i)))
                 delet=cursor.fetchall()
                 if len(delet)==0:
                     cursor.execute("select * from PLdata order by ID")
