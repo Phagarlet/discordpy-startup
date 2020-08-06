e59b9be58e1763f65cb006b2faaa8c80669e1d20
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-08-06 15:27:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -780,7 +780,7 @@ async def on_message(message):
                     cursor.execute("update TTQual set Qual=(%s) where PLID=(%s)",(qual,i))#Qual
                     con.commit
                     
-                    cursor.execute("select * TTQUal where {0}=(%s)".format(Qual),('資格保持',))
+                    cursor.execute("select * TTQUal where Qual=(%s)",(Qual),)
                     FQ=cursor.fetchall()
                     await message.channel.send(FQ)
             else:
