ff19a68c60f2952587aed4f8b040e8d0a96b3f84
Phagarlet <66621275+Phagarlet@users.noreply.github.com>
2020-09-02 05:10:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -893,13 +893,15 @@ async def on_message(message):
                     cursor.execute("SELECT * FROM s3history order by MID")
                     con.commit
                 cursor.execute("SELECT * FROM TTQual order by Wnow")
-                cursor.execute("SELECT * FROM TTQual where Qual=(%s)",('資格保持',))
+                cursor.execute("SELECT * FROM TTQual order by Wnow where Qual=(%s)",('資格保持',) )
                 FQ=cursor.fetchall()
                 for i in range(len(FQ)):
+                    FQID=FQ[i][0]
                     await message.channel.send(FQ[i])
                 cursor.execute("SELECT * FROM TTQual where Qual=(%s)",('次点保持',))
                 SQ=cursor.fetchall()
                 for i in range(len(SQ)):
+                    SQID=SQ[i][0]
                     await message.channel.send(SQ[i])
                 cursor.execute("SELECT * FROM s3history order by MID")
                 con.commit
